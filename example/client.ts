6e3a2a293eb7f2d90c740f4bca81111f711ceedc
Emilio <emilioplatzer@gmail.com>
2018-03-04 16:51:45 -0300

    Cuando se genera para node los .js tienen que ser es2017

---
+++ example/client.ts
@@ -0,0 +1,19 @@
+
+async function fetchAndShow(url:string, id:string):Promise<void>{
+    let element = document.getElementById(id);
+    try{
+        let response = await fetch(url);
+        element.style.color='green';
+        let content = await response.text();
+        element.textContent=content;
+    }catch(err){
+        element.style.color='red';
+        console.log(err);
+        element.textContent=err.message;
+    };
+}
+
+window.addEventListener('load', function(){
+    fetchAndShow('/dummy1','dummy');
+    fetchAndShow('/up-time','up-time');
+});
